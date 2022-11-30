24ec7720487fa4b11e94bc2bae50529be64c4e48
Raul Pires <raulpires224@gmail.com>
2022-11-30 16:19:21 -0300

    Adiciona cors

--- ApiConfitec/Program.cs
+++ ApiConfitec/Program.cs
@@ -28,6 +28,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.UseAuthorization();
 
 app.MapControllers();
